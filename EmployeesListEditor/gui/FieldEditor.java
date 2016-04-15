0c6e6e622140c1a56c1ac195e5ed0c6336faec1c
Ivan <vana.shimko@gmail.com>
2016-04-15 23:55:40 +0300

    Some naive error handling

--- EmployeesListEditor/gui/FieldEditor.java
+++ EmployeesListEditor/gui/FieldEditor.java
@@ -11,14 +11,16 @@ abstract class FieldEditor {
     protected Object object;
 
     protected abstract void writeToControl(Object object);
+
     protected abstract String readFromControl();
+
     public abstract JComponent getControl();
 
     void getValueFromObject() {
-        try{
+        try {
             Object fieldValue = fieldDescription.getGetter().invoke(object);
             writeToControl(fieldValue);
-        } catch (IllegalAccessException | InvocationTargetException e){
+        } catch (IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace();
         }
     }
@@ -26,12 +28,14 @@ abstract class FieldEditor {
     void saveValueToObject() {
         try {
             String controlData = readFromControl();
-            if (controlData.isEmpty()){
+            if (controlData.isEmpty()) {
                 return;
             }
             fieldDescription.setFieldValue(ReflectHelper.createPrimitiveObject(fieldDescription.getClassType(), controlData));
             fieldDescription.getSetter().invoke(object, fieldDescription.getFieldValue());
-        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | ClassNotFoundException e){
+        } catch (InvocationTargetException e){
+            JOptionPane.showMessageDialog(null, "Unable to save value to object", "Error", JOptionPane.ERROR_MESSAGE);
+        } catch (NoSuchMethodException | IllegalAccessException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
