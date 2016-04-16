81a4476e63047027ab0296744fbe53dea35e09dc
Ivan <vana.shimko@gmail.com>
2016-04-16 13:29:51 +0300

    Nimbus LaF

--- EmployeesListEditor/Main.java
+++ EmployeesListEditor/Main.java
@@ -3,11 +3,17 @@ package EmployeesListEditor;
 import EmployeesListEditor.gui.MainWindow;
 
 import javax.swing.*;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 
 public class Main {
     private static void setSystemLookAndFeel() {
         try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
         } catch (Exception e) {
             System.err.println("Error while changing Look and Feel");
         }
